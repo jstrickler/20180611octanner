ea2bb6c39455c4974ec938af5d56c575af613129
jstrickler <jstrickler@gmail.com>
2018-06-11 11:16:41 -0600

    bytes

--- bytes_examples.py
+++ bytes_examples.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python
+
+with open('DATA/salad.gif', 'rb') as salad_in:
+    data = salad_in.read()
+
+print(type(data))
+
+s = "foo\u00B0\n"  #  seq of CHARs
+b = b"bar\n"  # seq of BYTEs
+
+print(s.encode())
+print(b.decode())
